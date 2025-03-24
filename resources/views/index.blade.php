432e92dad16a99dd94009a3d9433082282b8f7f0
Anthony Silva <anthonyoficialcarlos@gmail.com>
2025-03-24 08:23:58 -0300

    as alteracoes foram salvas, sa

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aulas da 1229</title>
+</head>
+<body>
+    <p> <b> Olá, Porra!!</p>
+</body>
+</html>
